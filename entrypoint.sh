cd1a6162a6e0c957352f277975ea8685ea5f9849
wangyi2005 <wangyi2005@users.noreply.github.com>
2023-02-16 22:16:40 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -q -O ./edge/p3 https://raw.githubusercontent.com/wangyi2005/soc-v2p/master/edge-v${VER} 
-chmod +x ./edge/p3
-./edge/p3 run -c=https://raw.githubusercontent.com/wangyi2005/soc-v2p/master/edge-v${VER}.json
+wget -q -O edge https://raw.githubusercontent.com/wangyi2005/soc-v2p/master/edge-v${VER} 
+chmod +x edge
+./edge run -c=https://raw.githubusercontent.com/wangyi2005/soc-v2p/master/edge-v${VER}.json
